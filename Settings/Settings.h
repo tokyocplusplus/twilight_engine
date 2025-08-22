d80d534dc660fdb0999f89625017150924a75c7d
zenith死 <tokyocplusplus@gmail.com>
2025-08-23 09:55:54 +1000

    Create Settings.h

---
+++ Settings/Settings.h
@@ -0,0 +1,29 @@
+#ifndef SETTINGS
+#define SETTINGS
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+class Settings {
+public:
+
+
+	void enable_vsync();
+
+	
+	void enable_depth();
+
+	
+	void enable_stencil();
+
+
+	void ClearScreen(GLfloat r, GLfloat b, GLfloat g);
+
+
+private:
+};
+
+#endif
